de404e54bd3ffd7a95fd3977604fdb51379f0f6e
Dogukan Bakibaba <mehmet.bakibaba@gmail.com>
2026-08-30 18:09:29 -0400

    Add global exception ahndling

--- Gym.API/Program.cs
+++ Gym.API/Program.cs
@@ -4,17 +4,28 @@ using Gym.API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddProblemDetails();
 
-builder.Services.AddOpenApi();
-builder.Services.AddValidation();
+
 builder.Services.AddDbContext<GymContext>(options => options.UseSqlite("Data Source=gym.db"));
-
 builder.Services.AddScoped<ExerciseService>();
 builder.Services.AddScoped<WorkoutService>();
 
-
 var app = builder.Build();
+
 app.MapExerciseEndpoints();
 app.MapWorkoutEndpoints();
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        await Results.Problem(
+            statusCode: 500,
+            title: "An unexpected error occured."
+        ).ExecuteAsync(context);
+    });
+});
+
+
 
 app.Run();
